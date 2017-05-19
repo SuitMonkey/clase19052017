92f3032efeb2898052563372320ef17032cbaa1f
Francis <francisangeles3@hotmail.com>
2017-05-19 11:45:15 -0500

    Inicializando el primer dia de clases de web

---
+++ src/main/java/edu/pucmm/clase19052017/Main.java
@@ -0,0 +1,11 @@
+package edu.pucmm.clase19052017;
+
+/**
+ * Created by Francis Cáceres on 19/5/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo en Java");
+
+    }
+}
